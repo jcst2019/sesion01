e16063b09a7498546f5b070610b1bdc47bc8de3b
jcst2019 <juansolar_4@hotmail.com>
2020-02-10 21:33:16 -0500

    Mi primer commit

---
+++ src/main/java/sesion01/Hola.java
@@ -0,0 +1,19 @@
+package sesion01;
+
+import java.util.Scanner;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Bienvenido al curso");
+ //sysout
+ 
+ //Capturar valores de la consola
+ Scanner sc=new Scanner(System.in);
+ System.out.println("Cuál es tu nombre");
+ 
+	
+}
+
+}
